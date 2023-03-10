caed38a353e3b88c5c91fcea31cbf3a890c23cff
VoicilaIonut <mariusvoicila03@yahoo.com>
2023-03-10 13:37:36 +0200

    Add copy constructor, copy operator=, destructor and operator<<

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <fstream>
 #include <unordered_map>
 // TODO change all variables name to English :)
+// maybe clasa pentru bancomat?
+// maybe loan class
 
 enum Currency {
     RON,
@@ -13,8 +15,26 @@ enum Currency {
 
 class Card {
     std::string cod, data_expirare, cvv; // maybe import a date type
-public: 
+    
+public:
     Card() = default;
+
+    friend std::ostream& operator<<(std::ostream& os, const Card& card) {
+        // TODO: add all parameters
+        os << "Card cod: " << card.cod;
+        return os;
+    }
+
+    Card& operator=(const Card& card) {
+        // TODO: add all parameters
+        cod = card.cod;
+        std::cout << "operator= copiere Card\n";
+        return *this;
+    }
+    ~Card() {
+        std::cout << "Destroing card\n";
+    }
+
 };
 
 /*
@@ -25,17 +45,59 @@ Can have: multiple currency, multiple cards (normal, bronze, silver, golden, pla
 class User {
     std::string nume, cnp, iban, email, numar_telefon;
     std::unordered_map<Currency, float> currencyAccount; // maybe create a vector instead of a hash table for tests
-    std::vector<Card> carduri = {}; // this should be optional
+    std::vector<Card> carduri = {};                      // this should be optional
 
 public:
-    User()= default;
+    User() = default;
 
+    friend std::ostream& operator<<(std::ostream& os, const User& user) {
+        // TODO: add all parameters
+        os << "User name: " << user.nume;
+        return os;
+    }
+
+    User& operator=(const User& user) {
+        // TODO: add all parameters
+        nume = user.nume;
+        std::cout << "operator= copiere User\n";
+        return *this;
+    }
+    
+    ~User() {
+        std::cout << "Destroing User\n";
+    }
+   
+    /*
+    TODO add functions for:
+    exchange
+    adaugare card
+    plataCard
+    retragere
+    */
 };
-
 
 class AppAccount {
     User user;
     std::string password;
+
+public:
+    AppAccount() = default;
+    friend std::ostream& operator<<(std::ostream& os, const AppAccount& appAccount) {
+        // TODO: add all parameters
+        os << "User account: " << appAccount.user;
+        return os;
+    }
+
+    AppAccount& operator=(const AppAccount& appAccount) {
+        // TODO: add all parameters
+        user = appAccount.user;
+        std::cout << "operator= copiere AppAccount\n";
+        return *this;
+    }
+
+    ~AppAccount() {
+        std::cout << "Destroing AppAccount\n";
+    }
 };
 
 class Tranzactie {
@@ -43,31 +105,28 @@ class Tranzactie {
     float value;
     Currency currency;
     bool realizata;
+
 public:
-    Tranzactie()= default;
+    Tranzactie() = default;
 
+    friend std::ostream& operator<<(std::ostream& os, const Tranzactie& tranzactie) {
+        // TODO: add all parameters
+        os << "User1: " << tranzactie.user1 << " - User2 " << tranzactie.user2;
+        return os;
+    }
+
+    Tranzactie& operator=(const Tranzactie& tranzactie) {
+        // TODO: add all parameters
+        user1 = tranzactie.user1;
+        user2 = tranzactie.user2;
+        std::cout << "operator= copiere Tranzactie\n";
+        return *this;
+    }
+
+    ~Tranzactie() {
+        std::cout << "Destroing Tranzactie\n";
+    }
 };
-
-
-
-class main
-{
-private:
-    /* data */
-public:
-    main(/* args */);
-    ~main();
-};
-
-main::main(/* args */)
-{
-}
-
-main::~main()
-{
-}
-
-
 
 int main() {
     std::cout << "Hello, world!\n";
